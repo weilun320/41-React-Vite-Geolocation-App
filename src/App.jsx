a3f54dede25c4c5fefad9064bb7e9b914573e251
Wei Lun <weilun9320@hotmail.com>
2023-12-14 16:07:41 +0800

    Added geolocation info

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,33 @@
+import { useEffect } from "react";
+import { useState } from "react";
 
 function App() {
-  return (
-    <>
+  const [latitude, setLatitude] = useState(null);
+  const [longitude, setLongitude] = useState(null);
 
-    </>
+  useEffect(() => {
+    const success = (position) => {
+      setLatitude(position.coords.latitude);
+      setLongitude(position.coords.longitude);
+    };
+
+    const error = (error) => console.error(error);
+
+    const option = {
+      enableHighAccuracy: true,
+      timeout: 1000,
+      maximumAge: 0,
+    };
+
+    navigator.geolocation.getCurrentPosition(success, error, option);
+  });
+
+  return (
+    <div>
+      <h1>Geolocation App</h1>
+      <p>Latitude: {latitude}</p>
+      <p>Longitude: {longitude}</p>
+    </div>
   );
 }
 
